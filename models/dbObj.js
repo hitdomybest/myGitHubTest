fd2d4db91616ccc0f21da608aaddd6559891221d
hitdomybest <hit_domybest@sina.com>
2017-11-08 16:51:55 +0800

    ttt

--- models/dbObj.js
+++ models/dbObj.js
@@ -1,10 +1,19 @@
 
 var mysql=require("mysql");
 
+/*
 var dbObj=mysql.createConnection({
     host: "127.0.0.1",
     user: "root",
     password: "mysql",
+    database: "userpost"
+});
+*/
+
+var dbObj=mysql.createConnection({
+    host: "127.0.0.1",
+    user: "mao",
+    password: "userpassword123",
     database: "userpost"
 });
 
